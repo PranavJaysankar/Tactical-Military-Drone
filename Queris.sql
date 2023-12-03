6b744070f7e0e5ebc8261f565aee3f7b78a420aa
JOTHIRUBAN <jothiruban1512@gmail.com>
2023-12-03 18:27:58 +0530

    remove

--- Queris.sql
+++
@@ -1,8 +0,0 @@
-/*query for the table users*/
-
-CREATE TABLE users (
-    user_id bigserial PRIMARY KEY,
-    user_name varchar NOT NULL,
-    email varchar NOT NULL UNIQUE,
-    password varchar
-);
